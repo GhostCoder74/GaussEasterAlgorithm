129fae4c2613e306c82ed40ff75d68f8563261f9
Christian Klose <Christian Klose ghostcoder@ghosthouse.lan>
2025-11-26 20:30:03 +0100

    Updating SPDX-License-Identifier Headers

--- modules/calendar_data.py
+++ modules/calendar_data.py
@@ -12,33 +12,6 @@
 # Header added by https://github.com/GhostCoder74/Set-Project-Headers
 # -----------------------------------------------------------------------------
 
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-from datetime import datetime
-from utils import days_in_month
-from holidays import get_holiday
-
-def build_calendar(start_year: int, end_year: int) -> list:
-    """Return a full calendar list of dicts."""
-    data = []
-
-    for year in range(start_year, end_year + 1):
-        for month in range(1, 13):
-            for day in range(1, days_in_month(month, year) + 1):
-                date = datetime(year, month, day)
-                entry = {
-                    "year": year,
-                    "month": month,
-                    "day": day,
-                    "weekday": date.weekday() + 1,
-                    "week": date.isocalendar()[1],
-                    "holiday": get_holiday(year, month, day)
-                }
-                data.append(entry)
-
-    return data
-
-# SPDX-License-Identifier: GPL-3.0-or-later
 
 from datetime import datetime
 from utils import days_in_month
